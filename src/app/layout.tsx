8f034a03de1682403e3b97ec433243fa69df23c4
geovgy <gbh218@gmail.com>
2025-02-02 16:48:59 -0500

    Install and implement posthog

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Footer } from "@/components/Footer";
+import { CSPostHogProvider } from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,9 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
-        <div className="flex-grow" />
-        <Footer />
+        <CSPostHogProvider>
+          {children}
+          <div className="flex-grow" />
+          <Footer />
+        </CSPostHogProvider>
       </body>
     </html>
   );
